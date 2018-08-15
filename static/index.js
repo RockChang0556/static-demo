697362c1970c1701719f189fe6143d0d7a6f31fb
v_zhangpeng06 <v_zhangpeng06@baidu.com>
2018-08-15 16:42:25 +0800

    添加demo

--- static/index.js
+++ static/index.js
@@ -7,62 +7,32 @@ $(document).ready(function(){
         {
             title: '画廊幻灯片',
             details: '展示图片用的',
-            img: '',
-            url: ''
+            href: './demoList/画廊幻灯片/index.html',
+            img: './demoList/画廊幻灯片/index.png'
         },
         {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
+            title: '3D轮播图',
+            details: '3D轮播图3D轮播图',
+            href: './demoList/3Dbanner/index.html',
+            img: './demoList/3Dbanner/3Dbanner.png'
         },
         {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
+            title: '苹果商品展示',
+            details: '苹果商品展示 苹果商品展示',
+            href: './demoList/苹果商品展示/index.html',
+            img: './demoList/苹果商品展示/index.png'
         },
         {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
+            title: '字符画',
+            details: '字符组成的画 字符组成的画',
+            href: './demoList/字符画/index.html',
+            img: './demoList/字符画/index.png'
         },
         {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
-        },
-        {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
-        },
-        {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
-        },
-        {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
-        },
-        {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
-        },
-        {
-            title: '画廊幻灯片',
-            details: '展示图片用的',
-            img: '',
-            url: ''
+            title: '3D折纸菜单',
+            details: '3D折纸菜单 3D折纸菜单',
+            href: './demoList/3D折纸菜单/index.html',
+            img: './demoList/3D折纸菜单/index.png'
         },
     ]
     let _demoList = $('#demo-list');
@@ -70,10 +40,10 @@ $(document).ready(function(){
     let renderDemoList = () => {
         let html = ``;
         demoList.forEach((v, i) => {
-            let url = v.title ? `./demoList/${v.title}/index.png` : 'https://api.vtrois.com/image/500x400?text=No cover';
-            html += `<a class="demo-box" target="_blank" href="./demoList/${v.title}/index.html">
+            let img = v.img ? v.img : 'https://api.vtrois.com/image/500x400?text=No cover';
+            html += `<a class="demo-box" target="_blank" href="${v.href || ''}">
                 <div class="demo-box-img">
-                    <img src="${url}" alt="">
+                    <img src="${img}" alt="">
                 </div>
                 <div class="demo-box-title">
                     <h3>${v.title}</h3>
